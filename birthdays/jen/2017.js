95c463e821e8414d29b43f762939af92a2e22024
Matthew Kwong <mkwong.sh@mysummitps.org>
2017-06-01 02:27:02 +0000

    make Jen Co's birthday cay card

---
+++ birthdays/jen/2017.js
@@ -0,0 +1,14 @@
+
+/*Each part of the triangle*/
+$("#1").delay(150).fadeOut(750);
+$("#2").delay(550).fadeOut(750);
+$("#3").delay(750).fadeOut(750);
+$("#4").delay(1050).fadeOut(750);
+/*Wild World Communications*/
+$("#wwcs").delay(1350).fadeOut(750);
+/*Pit of Passion*/
+$("#pitOfPassion").delay(1650).fadeOut(750);
+/*Christmas*/
+$("#merryChristmas").delay(1950).fadeout(750);
+
+$("#background").delay(1050).fadeout(750);
